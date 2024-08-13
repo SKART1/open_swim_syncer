ce801b3f3fbbb9948cfc19a5cc0b39da5670e5ff
SKART1 <art.enikoff@gmail.com>
2024-08-13 18:02:36 +0300

    small improvements

--- copy_preserving_order.sh
+++ copy_preserving_order.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
+# Check if exactly two arguments were passed
+if [ "$#" -ne 2 ]; then
+    echo "Error: You must provide exactly 2 arguments."
+    exit 1
+fi
+
 src=$1
 dst=$2
 
-
-# Создаем целевую директорию, если она не существует
+# Create the target directory if it doesn't exist
 mkdir -p "$dst"
 
-# Получаем список файлов с датами создания и сортируем их
+# Get the list of files with their creation dates and sort them
 files=$(find "$src" -type f -print0 | xargs -0 stat --format '%W %n' | sort -n)
 
-# Копируем файлы в отсортированном порядке
+# Copy the files in sorted order
 while IFS= read -r line; do
-    # Извлекаем имя файла из строки
+    # Extract the file name from the line
     file=$(echo "$line" | awk '{print substr($0, index($0,$2))}')
-    # Копируем файл в целевую директорию
+    # Copy the file to the target directory
     cp "$file" "$dst"
     sleep 1
 done <<< "$files"
 
-echo "Файлы успешно скопированы в $dst в отсортированном порядке."
+echo "Files have been successfully copied to $dst in order preserving creation date order (which will result in sorted)"
--- preprocess_and_split.sh
+++ preprocess_and_split.sh
@@ -1,18 +1,40 @@
-input_file_name=$1
+# Check if exactly two arguments were passed
+if [ "$#" -ne 2 ]; then
+    echo "Error: You must provide exactly 2 arguments."
+    exit 1
+fi
+
+# Check if the second argument is an integer
+if ! [ "$2" -eq "$2" ] 2>/dev/null; then
+    echo "Error: The second argument must be an integer."
+    exit 1
+fi
+
+full_file_path=$1
 part_size_seconds=$2
 
-
 # Filename without extension
-file_name="${input_file_name%.*}"
+base_file_name=$(basename "$full_file_path")
+file_name="${base_file_name%.*}"
 # Extract file 'extension' only
-file_extension="${input_file_name##*.}"
+file_extension="${base_file_name##*.}"
+# Extract the directory path from the provided argument
+file_directory=$(dirname "$full_file_path")
 
+
+increased_sound=${file_directory}/${file_name}_150.${file_extension}
+normalized=${file_directory}/${file_name}_150_norm.${file_extension}
+splitted=${file_directory}/split/${file_name}_150_norm_part_.${file_extension}
 
 # Increase volume by 50%
-sox $input_file_name ${file_name}_150.${file_extension} vol 1.5
+echo "Increasing volume"
+sox ${full_file_path} ${increased_sound} vol 1.5
 
 # Normalize sound - avoid overload
-sox ${file_name}_150.${file_extension} ${file_name}_150_norm.${file_extension}  gain -n 
+echo "Normalizing"
+sox ${increased_sound} ${normalized}  gain -n 
 
 # Split
-sox ${file_name}_150_norm.${file_extension} ${file_name}_150_norm_part_.${file_extension} trim 0 $part_size_seconds : newfile : restart
+echo "Splitting"
+mkdir ${file_directory}/split
+sox ${normalized} ${splitted} trim 0 $part_size_seconds : newfile : restart
